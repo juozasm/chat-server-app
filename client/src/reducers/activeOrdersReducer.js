3caa6941926f1b76426a0abf5ae642a55b894e83
juozasm <31696919+juozasm@users.noreply.github.com>
2018-07-30 10:28:59 +0300

    Delete activeOrdersReducer.js

--- client/src/reducers/activeOrdersReducer.js
+++
@@ -1,8 +0,0 @@
-import * as types from '../actions/types';
-
-export default (state=[], action)=>{
-    switch (action.type){
-      case types.NEW_ORDER : return [action.payload,...state];
-      default : return state;
-    }
-}
